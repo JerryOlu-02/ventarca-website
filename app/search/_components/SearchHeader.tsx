777e7ca9ec05b168885cdeff6d30ede1ef88f1bb
JerryOlu-02 <joluwole20@gmail.com>
2025-11-27 18:42:01 +0100

    fix: searchParams

--- app/search/_components/SearchHeader.tsx
+++ app/search/_components/SearchHeader.tsx
@@ -3,7 +3,7 @@
 import Button from "@/components/common/Button";
 import SortImg from "@/public/icon/sort.svg";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useState } from "react";
+import { Suspense, useCallback, useState } from "react";
 
 export default function SearchHeader({
   noOfListings,
@@ -43,37 +43,41 @@ export default function SearchHeader({
   };
 
   return (
-    <section className="section section_header">
-      <div className="page_width search_header">
-        <h3>{industry ? industry : location ? location : "Curated For You"}</h3>
+    <Suspense>
+      <section className="section section_header">
+        <div className="page_width search_header">
+          <h3>
+            {industry ? industry : location ? location : "Curated For You"}
+          </h3>
 
-        <div className="search_sort">
-          <p className="sort_text">
-            Showing {noOfListings}+ results sorted by{" "}
-            <span>date published</span>
-          </p>
+          <div className="search_sort">
+            <p className="sort_text">
+              Showing {noOfListings}+ results sorted by{" "}
+              <span>date published</span>
+            </p>
 
-          <div className="sort_container">
-            <Button
-              onClick={toggleIsVisible}
-              type="button"
-              className="btn btn-secondary btn-small"
-            >
-              <SortImg />
-              SORT BY
-            </Button>
+            <div className="sort_container">
+              <Button
+                onClick={toggleIsVisible}
+                type="button"
+                className="btn btn-secondary btn-small"
+              >
+                <SortImg />
+                SORT BY
+              </Button>
 
-            <div className={`${isVisible && "visible"}`}>
-              <button onClick={() => sortBy("ASC")} type="button">
-                Ascending
-              </button>
-              <button onClick={() => sortBy("DESC")} type="button">
-                Descending
-              </button>
+              <div className={`${isVisible && "visible"}`}>
+                <button onClick={() => sortBy("ASC")} type="button">
+                  Ascending
+                </button>
+                <button onClick={() => sortBy("DESC")} type="button">
+                  Descending
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Suspense>
   );
 }
