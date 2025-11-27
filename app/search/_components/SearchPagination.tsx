45c8553652263f32f016866e46caec035b8410f8
JerryOlu-02 <joluwole20@gmail.com>
2025-11-27 18:47:56 +0100

    fix: searchParams 2

--- app/search/_components/SearchPagination.tsx
+++ app/search/_components/SearchPagination.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { Suspense, useCallback } from "react";
 
 export default function SearchPagination({
   noOfListings,
@@ -56,86 +56,24 @@ export default function SearchPagination({
 
   return (
     currentNoOfPages > 1 && (
-      <section className="section pagination_section">
-        <div className="page_width pagination_container">
-          {currentSelectedPage && (
-            <div className="pagination">
-              {currentSelectedPage === 1 ? null : (
-                <button onClick={() => goToPrev()} className="pagination_btn">
-                  Prev
-                </button>
-              )}
-
-              {/* DISPLAY FIRST PAGE WHEN THE CURRENT SELECTED PAGE IS NOT 1 AND THE TOTAL NUMBER OF PAGES IS NOT 1 */}
-              {currentSelectedPage === 1
-                ? null
-                : currentNoOfPages === 1
-                ? null
-                : noOfPages.slice(0, 1).map((pageNo) => (
-                    <button
-                      type="button"
-                      key={pageNo}
-                      onClick={() => goToPage(pageNo)}
-                      className={`pagination_btn ${
-                        pageNo === currentSelectedPage && "active"
-                      }`}
-                    >
-                      {pageNo}
-                    </button>
-                  ))}
-
-              {currentSelectedPage === 1 ? null : currentNoOfPages ===
-                currentSelectedPage ? (
-                <button type="button" className="pagination_btn">
-                  ...
-                </button>
-              ) : null}
-
-              {/* DISPLAY PAGE BEFORE ACTIVE PAGE IF THE CURRENT ACTIVE PAGE IS NOT 2 */}
-              {currentSelectedPage === 2
-                ? null
-                : noOfPages
-                    .slice(currentSelectedPage - 2, currentSelectedPage - 1)
-                    .map((pageNo) => (
-                      <button
-                        type="button"
-                        key={pageNo}
-                        onClick={() => goToPage(pageNo)}
-                        className={`pagination_btn ${
-                          pageNo === currentSelectedPage && "active"
-                        }`}
-                      >
-                        {pageNo}
-                      </button>
-                    ))}
-
-              {/* DISPLAY ACTIVE PAGE */}
-              {noOfPages
-                .slice(currentSelectedPage - 1, currentSelectedPage)
-                .map((pageNo) => (
-                  <button
-                    type="button"
-                    key={pageNo}
-                    onClick={() => goToPage(pageNo)}
-                    className={`pagination_btn ${
-                      pageNo === currentSelectedPage && "active"
-                    }`}
-                  >
-                    {pageNo}
+      <Suspense>
+        {" "}
+        <section className="section pagination_section">
+          <div className="page_width pagination_container">
+            {currentSelectedPage && (
+              <div className="pagination">
+                {currentSelectedPage === 1 ? null : (
+                  <button onClick={() => goToPrev()} className="pagination_btn">
+                    Prev
                   </button>
-                ))}
+                )}
 
-              {/* DISPLAY PAGE AFTER ACTIVE PAGE IF THE CURRENT ACTIVE PAGE IS NOT 1 and No OF PAGES IS NOT 2 */}
-              {currentSelectedPage == 1 && currentNoOfPages === 1
-                ? null
-                : currentNoOfPages === 2
-                ? null
-                : currentNoOfPages > 2 &&
-                  currentSelectedPage === currentNoOfPages - 1
-                ? null
-                : noOfPages
-                    .slice(currentSelectedPage, currentSelectedPage + 1)
-                    .map((pageNo) => (
+                {/* DISPLAY FIRST PAGE WHEN THE CURRENT SELECTED PAGE IS NOT 1 AND THE TOTAL NUMBER OF PAGES IS NOT 1 */}
+                {currentSelectedPage === 1
+                  ? null
+                  : currentNoOfPages === 1
+                  ? null
+                  : noOfPages.slice(0, 1).map((pageNo) => (
                       <button
                         type="button"
                         key={pageNo}
@@ -148,19 +86,35 @@ export default function SearchPagination({
                       </button>
                     ))}
 
-              {/* DISPLAY MORE ... ICON WHEN THE SELECTED PAGE IS NOT THE SAME AS THE CURRENT NUMBER OF PAGES */}
-              {currentNoOfPages === currentSelectedPage ? null : (
-                <button type="button" className="pagination_btn">
-                  ...
-                </button>
-              )}
+                {currentSelectedPage === 1 ? null : currentNoOfPages ===
+                  currentSelectedPage ? (
+                  <button type="button" className="pagination_btn">
+                    ...
+                  </button>
+                ) : null}
 
-              {/* DISPLAY LAST PAGE WHEN CURRENT NO OF PAGES IS NOT 1 AND WHEN CURRENT SELECTED PAGE IS NOT EQUAL TO THE CURRENT NUMBER OF PAGES */}
-              {currentNoOfPages == 1
-                ? null
-                : currentSelectedPage === currentNoOfPages
-                ? null
-                : noOfPages.slice(-1).map((pageNo) => (
+                {/* DISPLAY PAGE BEFORE ACTIVE PAGE IF THE CURRENT ACTIVE PAGE IS NOT 2 */}
+                {currentSelectedPage === 2
+                  ? null
+                  : noOfPages
+                      .slice(currentSelectedPage - 2, currentSelectedPage - 1)
+                      .map((pageNo) => (
+                        <button
+                          type="button"
+                          key={pageNo}
+                          onClick={() => goToPage(pageNo)}
+                          className={`pagination_btn ${
+                            pageNo === currentSelectedPage && "active"
+                          }`}
+                        >
+                          {pageNo}
+                        </button>
+                      ))}
+
+                {/* DISPLAY ACTIVE PAGE */}
+                {noOfPages
+                  .slice(currentSelectedPage - 1, currentSelectedPage)
+                  .map((pageNo) => (
                     <button
                       type="button"
                       key={pageNo}
@@ -173,24 +127,73 @@ export default function SearchPagination({
                     </button>
                   ))}
 
-              {currentSelectedPage === currentNoOfPages ? null : (
-                <button
-                  onClick={() => goToNext()}
-                  type="button"
-                  className="pagination_btn"
-                >
-                  Next
-                </button>
-              )}
-            </div>
-          )}
+                {/* DISPLAY PAGE AFTER ACTIVE PAGE IF THE CURRENT ACTIVE PAGE IS NOT 1 and No OF PAGES IS NOT 2 */}
+                {currentSelectedPage == 1 && currentNoOfPages === 1
+                  ? null
+                  : currentNoOfPages === 2
+                  ? null
+                  : currentNoOfPages > 2 &&
+                    currentSelectedPage === currentNoOfPages - 1
+                  ? null
+                  : noOfPages
+                      .slice(currentSelectedPage, currentSelectedPage + 1)
+                      .map((pageNo) => (
+                        <button
+                          type="button"
+                          key={pageNo}
+                          onClick={() => goToPage(pageNo)}
+                          className={`pagination_btn ${
+                            pageNo === currentSelectedPage && "active"
+                          }`}
+                        >
+                          {pageNo}
+                        </button>
+                      ))}
 
-          <p className="sort_text">
-            Showing {noOfListings}+ results sorted by{" "}
-            <span>date published</span>
-          </p>
-        </div>
-      </section>
+                {/* DISPLAY MORE ... ICON WHEN THE SELECTED PAGE IS NOT THE SAME AS THE CURRENT NUMBER OF PAGES */}
+                {currentNoOfPages === currentSelectedPage ? null : (
+                  <button type="button" className="pagination_btn">
+                    ...
+                  </button>
+                )}
+
+                {/* DISPLAY LAST PAGE WHEN CURRENT NO OF PAGES IS NOT 1 AND WHEN CURRENT SELECTED PAGE IS NOT EQUAL TO THE CURRENT NUMBER OF PAGES */}
+                {currentNoOfPages == 1
+                  ? null
+                  : currentSelectedPage === currentNoOfPages
+                  ? null
+                  : noOfPages.slice(-1).map((pageNo) => (
+                      <button
+                        type="button"
+                        key={pageNo}
+                        onClick={() => goToPage(pageNo)}
+                        className={`pagination_btn ${
+                          pageNo === currentSelectedPage && "active"
+                        }`}
+                      >
+                        {pageNo}
+                      </button>
+                    ))}
+
+                {currentSelectedPage === currentNoOfPages ? null : (
+                  <button
+                    onClick={() => goToNext()}
+                    type="button"
+                    className="pagination_btn"
+                  >
+                    Next
+                  </button>
+                )}
+              </div>
+            )}
+
+            <p className="sort_text">
+              Showing {noOfListings}+ results sorted by{" "}
+              <span>date published</span>
+            </p>
+          </div>
+        </section>
+      </Suspense>
     )
   );
 }
